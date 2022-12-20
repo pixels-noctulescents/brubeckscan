80f1c64085839c4e8a031eb8d2cbbc323c64cd33
Adam Phi Vo <adamphivo@gmail.com>
2022-12-20 19:29:53 +0100

    Fix typo in load request

--- packages/client/src/routes/+layout.ts
+++ packages/client/src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import type { LayoutLoad } from "./$types";
 
 export const load = (async ({ params, fetch }) => {
-  const response = await fetch("http://51.38.184.53:3771/api/networks/stats");
+  const response = await fetch("http://51.38.184.53:3770/api/networks/stats");
   const data = await response.json();
   return {
     title: "Hello world!",
