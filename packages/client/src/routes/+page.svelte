779085427fe47076f2bdba490b8c0024e28a592a
Adam Phi Vo <adamphivo@gmail.com>
2022-12-22 15:20:47 +0100

    Add landing

--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<div>
-  <p>Page</p>
+<div class="landing">
+  <div class="left">Left</div>
+  <div class="right">Right</div>
 </div>
+
+<style lang="scss">
+  .landing {
+    display: flex;
+    flex-direction: row;
+    height: calc(100vh - 65px);
+    width: 100%;
+  }
+
+  .left {
+    background-color: rgb(18, 22, 34);
+    color: white;
+    flex-grow: 1;
+  }
+
+  .right {
+    background-color: whitesmoke;
+    flex-grow: 1;
+  }
+</style>
