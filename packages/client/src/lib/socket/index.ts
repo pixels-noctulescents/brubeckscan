43331ec02544ab0d8f86f05077e3c5cd97ea958c
Adam Phi Vo <adamphivo@gmail.com>
2022-12-24 16:17:42 +0100

    Attempt to fix cors request on prod

--- packages/client/src/lib/socket/index.ts
+++ packages/client/src/lib/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-import { PUBLIC_API_BASE_URL } from "$env/static/public";
+import { PUBLIC_SOCKET_URL } from "$env/static/public";
 
-const socket = io(PUBLIC_API_BASE_URL);
+const socket = io(PUBLIC_SOCKET_URL);
 
 export { socket };
