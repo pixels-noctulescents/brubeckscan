abdcace66072e5448a834b582f8386aa8be858fd
Adam Phi Vo <adamphivo@gmail.com>
2023-01-07 22:48:15 +0100

    Change PM2 Settings

--- packages/client/src/lib/components/layout/Header/NetworkStats.svelte
+++ packages/client/src/lib/components/layout/Header/NetworkStats.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
   import { network } from "$lib/stores/network";
   import { usersCount } from "$lib/stores/usersCount";
+  import MdPerson from "svelte-icons/md/MdPerson.svelte";
 </script>
 
-<div>
+<div class="container">
   <p>Latest reward code | {$network.lastRewards[0].code}</p>
-  <p>Users | {$usersCount}</p>
+  <div class="stats">
+    <div>
+      {$usersCount}
+    </div>
+    <div class="icon">
+      <MdPerson />
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-  div {
+  .container {
     display: flex;
-    gap: 20px;
+    flex-direction: row;
+    gap: 50px;
+  }
+
+  .stats {
+    display: flex;
+    gap: 5px;
+  }
+  .icon {
+    width: 15px;
   }
 </style>
