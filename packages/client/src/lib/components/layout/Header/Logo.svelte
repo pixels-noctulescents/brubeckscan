9ce92a0d89ef0a11491781739c1752ebd7a558b6
Adam Phi Vo <adamphivo@gmail.com>
2023-01-08 17:56:04 +0100

    add header design

--- packages/client/src/lib/components/layout/Header/Logo.svelte
+++ packages/client/src/lib/components/layout/Header/Logo.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-  import logo from "$lib/assets/streamr/logo.svg";
-
-  const LOGO_ALT = "The Streamr Logo";
-  const STREAMR_URL = "https://streamr.network";
 </script>
 
 <div>
-  <a href={STREAMR_URL} target="_blank" rel="noreferrer">
-    <img src={logo} alt={LOGO_ALT} />
-  </a>
   <p class="logoText">BRUBECKSCAN</p>
 </div>
 
@@ -22,13 +15,5 @@
     letter-spacing: -5px;
     font-size: 22px;
     transition-duration: 0.3s;
-  }
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  img {
-    width: 25px;
   }
 </style>
