8fd0e59bf02f4e0e42cd4a117bd230ab67153093
Adam Phi Vo <adamphivo@gmail.com>
2023-01-12 11:40:51 +0100

    Move auth process to client hook

--- packages/client/src/hooks.client.ts
+++ packages/client/src/hooks.client.ts
@@ -1,1 +1,11 @@
 import { Socket } from "$lib/services/Socket";
+import EthereumProviderService from "$lib/services/EthereumProvider";
+
+
+(async () => {
+    try {
+        await EthereumProviderService.init();
+    } catch (e) {
+        console.log(e);
+    }
+})()
--- packages/client/src/routes/+layout.ts
+++ packages/client/src/routes/+layout.ts
@@ -1,11 +1,8 @@
-import EthereumProviderService from "$lib/services/EthereumProvider";
 import send from "$lib/send";
 import type { LayoutLoad } from "./$types";
 import type { Network } from "@brubeckscan/common/types";
 
 export const load = (async ({ params, fetch }) => {
-  await EthereumProviderService.init();
-
   const response = await send(`networks/stats`, "GET", undefined, fetch);
 
   if (response && response.status === "success") {
