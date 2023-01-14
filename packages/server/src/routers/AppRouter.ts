728218c829f3e23778f9cfd3ddeadb97e28df6bf
Adam Phi Vo <adamphivo@gmail.com>
2023-01-14 17:19:05 +0100

    Add Octokit cache

--- packages/server/src/routers/AppRouter.ts
+++ packages/server/src/routers/AppRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { sender } from "../utils/sender";
 import { Request, Response, NextFunction } from "express";
 import { octokit } from "../clients/octokit";
+import { cache } from "../clients/cache";
 
 const GH_OWNER = "adamphivo";
 const GH_REPO = "brubeckAPI";
@@ -10,9 +11,14 @@ const AppRouter = express.Router();
 
 AppRouter.get("/version", async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const cached: any = cache.get("appLatestRelease");
+
+        if (cached) return sender.success(res, { latestRelease: cached.data });
+
         const latestRelease = await octokit.rest.repos.getLatestRelease({ owner: GH_OWNER, repo: GH_REPO })
 
         if (latestRelease) {
+            cache.set("appLatestRelease", latestRelease, 60 * 30);
             return sender.success(res, { latestRelease: latestRelease.data }, 200);
         }
 
