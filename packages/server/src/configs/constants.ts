fee2c3e939065eeaca629b091f19fe6dc2a16b24
Adam Phi Vo <adamphivo@gmail.com>
2023-01-20 17:57:53 +0100

    Update mainnet timestamp

--- packages/server/src/configs/constants.ts
+++ packages/server/src/configs/constants.ts
@@ -2,7 +2,7 @@ const constants = {
   DATA_CONTRACT: "0x3a9a81d576d83ff21f26f325066054540720fc34",
   DATA_GRAPH_URL: "https://api.thegraph.com/subgraphs/name/streamr-dev/data-on-polygon",
   STREAMR_ETH_ADDRESS: "0x3979f7d6b5c5bfa4bcd441b4f35bfa0731ccfaef",
-  MAINNET_TIMESTAMP: 1640995200,
+  MAINNET_TIMESTAMP: 1643673600,
   PAIRS: ["BTCUSDT", "DATAUSDT", "EURUSDT"],
   BINANCE_PRICE_TICKER_URL: "https://api.binance.com/api/v3/ticker/price?symbol=",
   BRUBECK_APY: "https://brubeck1.streamr.network:3013/apy",
